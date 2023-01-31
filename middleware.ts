e0d05e6783317b2ebadbeb3971f7b9bbc40dfc3c
pedro772 <plucena17230@gmail.com>
2023-01-31 20:50:40 -0300

    feat: add middleware edge function

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @next/next/no-server-import-in-page
+import { NextResponse } from "next/server";
+
+const signedInPages = ["/", "/playlist", "/library"];
+
+export default function middleware(req) {
+  if (signedInPages.find((page) => page === req.nextUrl.pathname)) {
+    const token = req.cookies.TRAX_ACCESS_TOKEN;
+
+    if (!token) {
+      return NextResponse.redirect(new URL("/signin", req.url));
+    }
+  }
+}
