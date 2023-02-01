8c72472ebe4e1bb7de1461ec8720a684d12a1de1
pedro772 <plucena17230@gmail.com>
2023-02-01 09:41:00 -0300

    feat: add gradientLayout component and home page layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,18 @@
+import GradientLayout from "../components/GradientLayout";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <GradientLayout
+      color="green"
+      roundImage
+      subtitle="Profile"
+      title="Pedro Lucena"
+      description="15 public playlists"
+      image="https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png?dl=0"
+    >
+      <div>Home Page</div>
+    </GradientLayout>
+  );
 };
 
 export default Home;
