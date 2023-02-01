2b6bb897a22774fd75d50655fa03052f2977f1a2
pedro772 <plucena17230@gmail.com>
2023-01-31 21:56:20 -0300

    fix: middleware token assignment

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ const signedInPages = ["/", "/playlist", "/library"];
 
 export default function middleware(req) {
   if (signedInPages.find((page) => page === req.nextUrl.pathname)) {
-    const token = req.cookies.TRAX_ACCESS_TOKEN;
+    const token = req.cookies.get("TRAX_ACCESS_TOKEN");
 
     if (!token) {
       return NextResponse.redirect(new URL("/signin", req.url));
